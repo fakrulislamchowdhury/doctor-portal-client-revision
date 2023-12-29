ad7628ecb48506b60ef27ea372885d4a5c683e10
Fakrul Islam <fislamc6@gmail.com>
2023-12-29 20:46:28 +0600

    pages added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navigation from './Pages/Shared/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navigation></Navigation>
+        <Switch>
+          <Route exact path='/home'>
 
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
